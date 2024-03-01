77d040ac08a5fec875e66c80b4b9653081812e45
meap02 <79549780+meap02@users.noreply.github.com>
2024-03-01 00:24:34 -0800

    Added models and movement

---
+++ Household Volcano/Assets/PlayerController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public int sensitivity = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(10, 0, 0);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(-10, 0, 0);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(0, 0, -10);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(0, 0, 10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space was pressed!");
+        }
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(Vector3.up * sensitivity * Time.deltaTime * Input.GetAxis("Mouse X"));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger enter " + other.transform.name);
+    }
+}
